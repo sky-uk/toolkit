979e205c3156536d955dec967e8404c49ae3149d
Stefan McCready <stefan.mccready@sky.uk>
2020-08-25 21:58:21 +0100

    chore(core): remove preview folder

--- preview/src/index.js
+++
@@ -1,8 +0,0 @@
-import { components, componentsUndocumented } from './js/data';
-import { renderHTML, renderListItems } from './js/render';
-
-import './index.scss';
-
-document.getElementById('components-list').innerHTML = renderListItems(components);
-document.getElementById('components-undocumented-list').innerHTML = renderListItems(componentsUndocumented);
-document.getElementById('main').innerHTML = renderHTML(components);
--- preview/src/js/utils.js
+++
@@ -1,5 +0,0 @@
-const capitalizeFirstLetter = str => `${str[0].toUpperCase()}${str.slice(1)}`;
-
-module.exports = {
-  capitalizeFirstLetter,
-};
